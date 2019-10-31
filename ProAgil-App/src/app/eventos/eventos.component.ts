4fa5acdfc314a781ca9b8ec475a0e6108767bdfe
bfiliano <brunofiliano@gmail.comm>
2019-10-30 22:26:34 -0400

    Adicionando Classes

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -7,13 +7,32 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-
+  _filtroLista: string;
   eventos: any = [];
   imagemLargura = 50;
   imagemMargin = 2;
   mostrarImagem = false;
+  eventosFiltrados: any = [];
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEvento(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEvento(filtrarPor: string) {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   ngOnInit() {
     this.getEventos();
@@ -23,14 +42,14 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-getEventos() {
-
-  this.http.get('http://localhost:5000/api/values').subscribe(
-    response => { this.eventos = response; },
-    error => {
-      console.log(error);
-    }
-  );
-}
-
+  getEventos() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {
+        this.eventos = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
